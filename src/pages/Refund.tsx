bfe91a95bf1169be2ee5d12fd5cb76f30d12c52c
Gabriel Sarti <gabriel_rti@hotmail.com>
2026-06-25 13:31:45 -0300

    add: Select and proprieties category

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,7 +1,11 @@
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
+import { useState } from "react";
 
 export function Refund(){
+    const [category, setCategory] = useState("")
+
     return <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
         <header>
             <h1 className="text-xl font-bold text-gray-100">Solicitação de reembolso</h1>
@@ -17,6 +21,17 @@ export function Refund(){
          <Select
          required
          legend="Categoria"
-         />
+         value={category}
+         onChange={(e) => setCategory(e.target.value)}
+         >
+            {
+                CATEGORIES_KEYS.map((category) => (
+                    <option key={category} value={category}>
+                        {CATEGORIES[category].name}
+
+                    </option>
+                ))
+            }
+         </Select>
     </form>
 }
